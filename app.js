05030b855fd63deb2cfe39e01aef28f17dbb3701
Guilherme Leal <lealhugui@gmail.com>
2016-08-29 14:53:30 -0300

    removed the mock data from the app, and placed into data.json file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 'use strict'
 
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var app = express();
+
+
 
 app.use('/public/css', express.static(__dirname + '/public/css'));
 app.use('/public/js', express.static(__dirname + '/public/js'));
@@ -11,36 +15,38 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname+'/index.html');
 });
 
-const MY_DATA = [
-    {id:1, desc:"abc1", value:1.2},
-    {id:2, desc:"abc2", value:1.3},
-    {id:3, desc:"abc3", value:1.4}
-]
+const MY_DATA_FILE = path.join(__dirname, 'data.json');
 
 //Main rest endpoints
 app.get('/api/:resource', function(req, res){
-    let ret = { 
-      in: req.params['resource'],
-      data: MY_DATA
-    };
-    res.json(ret);
+    fs.readFile(MY_DATA_FILE, function(err, d){
+        d = JSON.parse(d);
+        let ret = { 
+        in: req.params['resource'],
+        data: d
+        };
+        res.json(ret);
+    });
 });
 
 //Main rest endpoints
 app.get('/api/:resource/:id', function(req, res){
-    let this_data = {};
-    
-    for(i=0; i<MY_DATA.length;i++){                
-        if(MY_DATA[i].id.toString()===req.params.id.toString()){
-            this_data = MY_DATA[i];
-            break;
+    fs.readFile(MY_DATA_FILE, function(err, d){
+        d = JSON.parse(d);
+        let this_data = {};
+        
+        for(i=0; i<d.length;i++){                
+            if(d[i].id.toString()===req.params.id.toString()){
+                this_data = d[i];
+                break;
+            }
         }
-    }
-    let ret = { 
-      in: req.params['resource']+"/"+req.params['id'],
-      data: this_data
-    };
-    res.json(ret);
+        let ret = { 
+        in: req.params['resource']+"/"+req.params['id'],
+        data: this_data
+        };
+        res.json(ret);
+    });
 });
 
 app.listen(3000, function () {
